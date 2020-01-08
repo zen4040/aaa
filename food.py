95f26fd34e9adf17fd2e12d649fc268181096906
zen4040 <59008143+zen4040@users.noreply.github.com>
2020-01-08 13:49:50 +0900

    Update food.py

--- food.py
+++ food.py
@@ -9,4 +9,4 @@ class Food(MenuItem):
         return self.name + ': ¥' + str(self.price) + ' (' + str(self.calorie) + 'kcal)'
     
     def calorie_info(self):
-        print(str(self.calorie) + 'kcalです')
+        print(str(self.calorie) + 'kcal.')
